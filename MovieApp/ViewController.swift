a5040603a466363fe5f9e743e968bbab52c10fea
Tahir Uzelli <58370244+tahiruzelli@users.noreply.github.com>
2022-04-08 00:51:48 +0300

    kullanacagim pod eklendi mimarinin temelleri atildi

--- MovieApp/ViewController.swift
+++ MovieApp/ViewController.swift
@@ -6,12 +6,21 @@
 //
 
 import UIKit
+import NVActivityIndicatorView
 
 class ViewController: UIViewController {
-
+    var activityIndicator : NVActivityIndicatorView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let xAxis = self.view.center.x
+        let yAxis = self.view.center.y
+        let frame = CGRect(x: (xAxis - 22.5), y: (yAxis - 45), width: 45, height: 45)
+        activityIndicator = NVActivityIndicatorView(frame: frame)
+        activityIndicator.type = .ballTrianglePath
+        activityIndicator.color = UIColor.red
+
+        self.view.addSubview(activityIndicator)
+        activityIndicator.startAnimating()
     }
 
 
